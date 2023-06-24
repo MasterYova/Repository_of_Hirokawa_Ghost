7bbb7007c1a56c33fde46f82dc318a8c6a637722
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-06-24 18:37:18 +0200

    Update patch.js

--- en-eastern/patch.js
+++ en-eastern/patch.js
@@ -1,14 +1,14 @@
 {
     "id": "en-eastern",
   "servers": [
-    "https://srv.thpatch.net/yova/en-eastern"
+    "https://mirrors.thpatch.net/yova/en-eastern/"
   ],
   "dependencies": [
     "nmlgc/base_tsa",
     "nmlgc/base_tasofro",
     "nmlgc/script_latin",
-    "nmlgc/eastern_name_order_hepburn",
-    "thpatch/lang_en"
+    "thpatch/lang_en",
+    "nmlgc/eastern_name_order_hepburn"
   ],
   "title": "Character names in the eastern name order for the english patch",
   "update": true
