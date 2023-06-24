ad38da17316980d6a4381c41c4b8cbb34096846c
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-06-24 17:48:20 +0200

    Add files via upload

---
+++ en-eastern/patch.js
@@ -0,0 +1,15 @@
+{
+    "id": "en-eastern",
+  "servers": [
+    "https://srv.thpatch.net/yova/en-eastern"
+  ],
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro",
+    "nmlgc/script_latin",
+    "nmlgc/eastern_name_order_hepburn",
+    "lang_en"
+  ],
+  "title": "Character names in the eastern name order for the english patch",
+  "update": true
+}
