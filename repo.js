4cd01550108c03dab79b1eb3402b416c90c66532
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-06-24 17:39:16 +0200

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,14 @@
+{
+    "id": "yova",
+    "title": "Yova's Repository",
+    "contact": "yoyova@yova.8shield.net",
+    "servers": [
+        "https://github.com/MasterYova/Repository_of_Hirokawa_Ghost"
+    ],
+    "neighbors": [
+        "https://srv.thpatch.net/"
+    ],
+    "patches": {
+        "en-eastern": "Character names in the eastern name order for the english patch"
+      }
+}
