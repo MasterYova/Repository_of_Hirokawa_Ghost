7da5e7b612705b9e3adb79a8384c81a648399e79
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-06-24 18:31:03 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -3,7 +3,6 @@
     "title": "Yova's Repository",
     "contact": "yoyova@yova.8shield.net",
     "servers": [
-        "https://github.com/MasterYova/Repository_of_Hirokawa_Ghost/master/",
         "https://mirrors.thpatch.net/yova/"
     ],
     "neighbors": [
