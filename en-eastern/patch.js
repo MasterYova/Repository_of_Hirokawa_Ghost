0f850121163a738737265af17863a39eac8072e5
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-06-25 15:26:31 +0200

    Update patch.js

--- en-eastern/patch.js
+++ en-eastern/patch.js
@@ -8,6 +8,7 @@
     "nmlgc/base_tasofro",
     "nmlgc/script_latin",
     "thpatch/lang_en",
+    "nmlgc/western_name_order"
     "nmlgc/eastern_name_order_hepburn"
   ],
   "title": "Character names in the eastern name order for the english patch",
