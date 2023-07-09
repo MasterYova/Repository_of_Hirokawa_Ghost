2d71927168f8d4c2e9bbc19f2ac18ef5ce86a0e7
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-09 10:36:39 +0200

    Create patch.js

---
+++ kuchko/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_sr"
+	],
+	"id": "kuchko",
+	"servers": [
+		"https://mirrors.thpatch.net/yova/kuchko],
+    ],
+	"title": "Prevod Alisine i Marisine \"bitch\" rečenice na srpski(latinicu) u TH08."
+}
