2b32bbd1fec8b284cbf606ed0153451d65b17999
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-14 11:34:23 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -9,7 +9,7 @@
         "https://srv.thpatch.net/"
     ],
     "patches": {
-        "en-eastern": "Japanese characters' names in the eastern name order(last name first) for the english patch",",
+        "en-eastern": "Japanese characters' names in the eastern name order(last name first) for the english patch",
         "kuchko": "Prevod Alisine i Marisine \"bitch\" rečenice na srpski(latinicu) u TH08."
       }
 }
