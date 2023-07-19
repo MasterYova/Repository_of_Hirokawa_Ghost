5fbec01dece3be0f2414ae91e03eef0c05ee5c3e
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-19 08:21:09 +0200

    Add files via upload

---
+++ en-eastern/th18/abilities.js
@@ -0,0 +1,297 @@
+{
+    "8_0": [
+        "Hakurei Reimu's ability to shoot homing shots.",
+        "A simple and convenient card to increase attack power.",
+        "",
+        "Just when did they copy a power like this?",
+        "That it's on the market is pretty scary..."
+    ],
+    "9_0": [
+        "Hakurei Reimu's ability to shoot youkai-vanquishing needles.",
+        "A simple, easy-to-use card with absolutely no special traits.",
+        "For youkai, Reimu's needles must surely be an",
+        "itch-scratcher that feels achingly good.",
+        "That's why they keep happily getting hit with them."
+    ],
+    "10_0": [
+        "Kirisame Marisa's ability to shoot a laser.",
+        "One of the highest-firepower Equipment Cards there is.",
+        "",
+        "There are many humans and youkai who wanted to try",
+        "shooting a laser, so it's a fairly popular card."
+    ],
+    "11_0": [
+        "Kirisame Marisa's ability to shoot missiles.",
+        "There are times when a slower shot can be useful as well.",
+        "",
+        "...There was a time when I thought as much, too.",
+        "A useful card that simply boosts your attack power."
+    ],
+    "12_0": [
+        "Izayoi Sakuya's ability to throw knives.",
+        "Throwing knives that can be shot left and right.",
+        "",
+        "You are not qualified to be a maid",
+        "until you can aim and hit the fairies."
+    ],
+    "13_0": [
+        "Izayoi Sakuya's ability to throw ricocheting knives.",
+        "A card that concentrates its attack power on specific points.",
+        "",
+        "Your shot becomes flashier, which feels great.",
+        "The flashier the attack, the better, right?"
+    ],
+    "14_0": [
+        "The Moriya Shrine's popular amulet.",
+        "Can shoot exploding frog shots.",
+        "One of Kochiya Sanae's abilities...",
+        "or rather, something Suwako gave to her.",
+        "A good-luck charm to ensure your safe return home."
+    ],
+    "15_0": [
+        "Can shoot snake bullets that bite at the enemies.",
+        "One of Kochiya Sanae's abilities...",
+        "",
+        "or rather, something Kanako Yasaka gave to her.",
+        "People say that holding this charm gives you a healthier body."
+    ],
+    "16_0": [
+        "Konpaku Youmu's phantom. Attacks while following right behind you.",
+        "",
+        "It's half of Youmu's ghost half, not half of Youmu herself. So it's 1/4 of her.",
+        "Being possessed by a phantom is typically nothing to be glad about,",
+        "but since it attacks things for you, it's something you can more-or-less rely on."
+    ],
+    "19_0": [
+        "Matara Okina's ability to unlock the door in one's back.",
+        "Erases bullets behind you, and can deal damage as well.",
+        "For living creatures, the back is both a weak point and a utopia.",
+        "Does it hide a wise sage, to whom you can entrust your back?",
+        "Or does it hide Tenma, residing in paradise?"
+    ],
+    "20_0": [
+        "Houjuu Nue's ability. Summons a UFO flying around you.",
+        "The UFO becomes a shield, capable of erasing bullets.",
+        "You've seen plenty of photographs of UFOs, right?",
+        "But there's no space war going on.",
+        "UFOs fly around just because they feel like it."
+    ],
+    "21_0": [
+        "Kawashiro Nitori's ability, which embodies her policy of",
+        "\"everything that people drop is mine for the taking!\"",
+        "Your item collection radius shall expand dramatically!",
+        "Kappa can stretch their arms out, so this is clearly",
+        "taking advantage of that fact to scoop up items."
+    ],
+    "22_0": [
+        "Yasaka Kanako's ability to collect every last thing offered to the mountain.",
+        "The item autocollect line is dramatically lowered!",
+        "",
+        "\"All offerings to the mountain belong to I!\"",
+        "Ah, it has such a self-centered ring to it!"
+    ],
+    "23_0": [
+        "Yagokoro Eirin's ability to negate a death in exchange",
+        "for two Spell Cards (or one, if you only have one left).",
+        "A medicine that's popular with demographics who have issues with",
+        "elixirs of immortality, but will happily accept elixirs of long life.",
+        "There's really not much difference, though."
+    ],
+    "24_0": [
+        "Inaba Tewi's ability. A tiny bit of good luck visits you upon getting hit.",
+        "Your window to use a Spell Card upon death gets longer,",
+        "and you won't lose any money upon dying, either.",
+        "A good-luck charm that helps you avoid losing lives,",
+        "as quick and cunning as a rabbit."
+    ],
+    "25_0": [
+        "Kurokoma Saki's ability to increase the power of your regular shot.",
+        "A useful ability that helps make you stronger even if your Power is low.",
+        "No matter how much one uses their head or devises strategies,",
+        "everyone's bound to surrender in the face of overwhelming strength.",
+        "Attack power's what really matters in the end, y'know?"
+    ],
+    "26_0": [
+        "Hijiri Byakuren's ability to massively buff your Spell Cards.",
+        "The cooldown time for Active Cards also decreases.",
+        "You can attack enemies with your peashooter all you like, but in the end,",
+        "you're bound to get cornered in no time and beat 'em with a huge release of magic.",
+        "So if you're gonna power something up, it's gotta be your Spell Cards!"
+    ],
+    "27_0": [
+        "Komeiji Koishi's ability to prevent you from crashing into enemies.",
+        "You also lose less power when you get hit.",
+        "",
+        "Is it just me, or is nobody noticing me?",
+        "Huh, okay. That's convenient."
+    ],
+    "28_0": [
+        "Moriya Suwako's ability to make your shot sometimes explode.",
+        "Your main shot will sometimes catch small-fry in the blast.",
+        "People often talk about exploding frogs with firecrackers back in the day.",
+        "But you think they don't go around exploding on their own, don't you?",
+        "As a matter of fact, they do explode naturally."
+    ],
+    "29_0": [
+        "Shameimaru Aya's ability to enable super-fast movement.",
+        "You can move at extreme speeds while not firing any shots.",
+        "During high-speed movement, your hitbox becomes extremely small,",
+        "and you become very briefly invincible when beginning to move.",
+        "It's the ability to cause hitbox crimes that only a tengu can."
+    ],
+    "30_0": [
+        "Haniyasushin Keiki's ability. Your options become able to erase bullets.",
+        "To beat the enemy, you need trustworthy allies. Speaking of which,",
+        "your options are always right by your side, aren't they?",
+        "If they could become shields that erase bullets...!",
+        "(Unfortunately they each have a cool-down time.)"
+    ],
+    "31_0": [
+        "Houraisan Kaguya's ability to produce a",
+        "Spell Card item whenever you get hit.",
+        "Nobody needs impractical things like five secret treasures.",
+        "What do you think the most crucial thing for an eternal life is?",
+        "...That's right. Savings."
+    ],
+    "32_0": [
+        "Futatsuiwa Mamizou's ability to constantly keep your Power high.",
+        "You receive 1.0 Power upon acquiring this card,",
+        "and if you die, you can only lose Power down to 3.0 at minimum.",
+        "P Items are basically just dead weight, yeah?",
+        "Scurryin' around to grab those things is playing right into the enemies' hands.",
+        "Leave stuff like item-scrounging to your underlings."
+    ],
+    "33_0": [
+        "Saigyouji Yuyuko's ability to sometimes erase enemy bullets via grazing.",
+        "Ghosts do not fear death. Rather, they cannot fear death.",
+        "Grazing means accepting the fear of death.",
+        "For a ghost who couldn't possibly fear death,",
+        "grazing should pose no threat at all."
+    ],
+    "34_0": [
+        "Kicchou Yachie's ability to make enemies produce extra money.",
+        "Steal meager amounts of money from fairies with a demeanor",
+        "that's polite, yet also steals one's will to resist.",
+        "What do you need when it's time to buy an Ability Card?",
+        "What's the most important thing in one's life? You should know the answer."
+    ],
+    "35_0": [
+        "Shiki Eiki, Yamaxanadu's ability to spend 200 of",
+        "your money and pretend that a death never happened.",
+        "I know everything that you did in life before you died.",
+        "...Since you have that much money in your possession,",
+        "it seems that you're worthy enough to get a second chance at life."
+    ],
+    "36_0": [
+        "Yatadera Narumi's ability to gradually gain lives over time.",
+        "You receive 1 more life upon acquiring this card,",
+        "and every time you clear a stage, you get a life fragment.",
+        "Thank you for always providing for the jizo-sama statues!",
+        "You want a healthy body in return, don't you? Don't you?"
+    ],
+    "38_0": [
+        "Goutokuji Mike's ability to improve product lineups.",
+        "The variety of cards you can buy from bosses will increase!",
+        "",
+        "The maneki-neko living at Goutoku-ji are normally white cats,",
+        "so she had a tough time due to being born a calico cat."
+    ],
+    "39_0": [
+        "Yamashiro Takane's ability. You can buy cards for slightly cheaper.",
+        "",
+        "The ironclad rule of commerce: buy cheap, sell high.",
+        "But that doesn't mean you have to buy cheap products.",
+        "Yamawaro have techniques that let them stock high-quality goods at low prices."
+    ],
+    "40_0": [
+        "Komakusa Sannyo's ability. Your stock of lives slowly increases.",
+        "A life fragment appears every time you capture a Spell Card.",
+        "",
+        "When you're doing good, you get more lives: an ability for really livin' it up.",
+        "\"Go 'round three times, then sit down for a smoke.\""
+    ],
+    "41_0": [
+        "Yakumo Yukari's ability to let you freely warp between screen edges.",
+        "Press the card-use button on the edge of the screen, and you'll warp to the other edge.",
+        "A card that's popular amongst many humans and youkai,",
+        "because \"what is a screen edge, anyway?\"",
+        "is a great topic for getting people's interest."
+    ],
+    "42_0": [
+        "Sukuna Shinmyoumaru's ability to change enemy bullets into money.",
+        "A brave inchling vanquished a giant oni.",
+        "The \"Miracle Mallet\" that the oni dropped in its wake",
+        "was an object that could grant any wish imaginable.",
+        "\"Oh, if only the bullets were all money...!\""
+    ],
+    "43_0": [
+        "Hinanawi Tenshi's ability to throw a keystone, which acts as a shield and erases bullets.",
+        "A reliable stone that can erase a considerable number of bullets.",
+        "\"Offense is the greatest defense?\"",
+        "In the world of danmaku, where survival Spell Cards exist,",
+        "defense is the greatest offense of all!"
+    ],
+    "47_0": [
+        "Reiuji Utsuho's ability to burn everything to ash.",
+        "Ultimate attack power, endless invincibility time,",
+        "a way-too-long cooldown, and it eats up your own Power to boot.",
+        "Nudging your way through danmaku is for wimpy showboaters!",
+        "If you're really powerful, you just need to burn everything down."
+    ],
+    "48_0": [
+        "An ability card that summons Lily White when used.",
+        "She's an item carrier.",
+        "She either holds a Spell Card fragment or a life fragment.",
+        "But be careful, because she shoots bullets at you too!",
+        "Every time you summon her, she gets stronger!"
+    ],
+    "49_0": [
+        "Horikawa Raiko's ability to wipe away enemy bullets at close range.",
+        "It's plain, but you can thwack it over and over!",
+        "What do you think enemy bullets are made of?",
+        "They're waves. Ergo, there's nothing strange about",
+        "being able to cancel them out with sound waves!"
+    ],
+    "50_0": [
+        "Usami Sumireko's ability to reflect enemy bullets (except lasers).",
+        "If you had psychic powers, which one would you use?",
+        "Teleportation? Telepathy?",
+        "Hmm, I guess I'd have to go with some nice psychokinesis.",
+        "I mean, you can take care of everything besides that with technology, right?"
+    ],
+    "51_0": [
+        "Tamatsukuri Misumaru's ability to make a magatama appear on each side.",
+        "They erase bullets coming directly from either side of you.",
+        "Whenever they dig up ancient artifacts of unknown purpose,",
+        "humans immediately imagine that they were used for religious purposes.",
+        "Even though they actually had a much more practical use."
+    ],
+    "52_0": [
+        "Kudamaki Tsukasa's ability to fire a Spell Card in exchange for Power.",
+        "When you're surrounded by danmaku and sense danger--",
+        "when you think \"it's over!\", \"I'm finished!\"-- what would you do?",
+        "Why, yes. You'd use a Spell Card. That's exactly what you'd do.",
+        "This card lets you fire off as many Spell Cards as you want."
+    ],
+    "53_0": [
+        "Iizunamaru Megumu's ability to increase your Power any number of times.",
+        "You receive 1.0 Power upon using this card,",
+        "and when you're at full power, you receive a Spell Card fragment.",
+        "Why are you collecting Power after setting foot on the mountain?",
+        "You should pack tons of food to begin with, and then set out."
+    ],
+    "54_0": [
+        "Himemushi Momoyo's ability to make you stronger if you don't die or bomb.",
+        "You gradually get stronger each time you defeat an enemy,",
+        "but if you die or use a Spell Card, you'll go back to normal.",
+        "You have more reason to be frugal with your Spell Cards, which gives you a tingly feeling.",
+        "That's the centipede's toxin at work."
+    ],
+    "55_0": [
+        "A magic item that lets you breathe even in low-oxygen areas.",
+        "Some humans think that dungeons have breathable oxygen",
+        "just like everywhere else, but that's a big misassumption.",
+        "The only ones who can breathe easy in dungeons are video game characters.",
+        "＊Always equipped during the Extra Stage",
+        "＊Can't be equipped outside of the Extra Stage"
+    ]
