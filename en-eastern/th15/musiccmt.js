b48bd30472aaa2991a7fa3ec4252d101f8d10c67
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:54:23 +0200

    Add files via upload

---
+++ en-eastern/th15/musiccmt.js
@@ -0,0 +1,12 @@
+{
+    "9": [
+        "@",
+        "　",
+        "Kishin Sagume's theme.",
+        "",
+        "A theme song for a mysterious person who's not quite an enemy or an ally, a god or an oni.",
+        "Hence, I composed it with a rather mysterious mood.",
+        "",
+        "Actually, she's a type of amanojaku too so that makes her the second one."
+    ]
+}
