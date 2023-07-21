505fa73d1b544bf98a19ef356741fd30a06ea241
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 12:12:17 +0200

    Add files via upload

---
+++ en-eastern/th19/musiccmt.js
@@ -0,0 +1,27 @@
+{
+    "2": [
+        "@",
+        "　",
+        "Hakurei Reimu's and the Story Mode's opening theme.",
+        "",
+        "Reminiscent of Reimu, this is the song that sets the tone of the game.",
+        "Fun, fluffy and cute — wouldn't it be nice if the world was made like that?",
+        "...is what was on my mind when I composed this song."
+    ],
+    "4": [
+        "@",
+        "　",
+        "Komano Aunn's theme.",
+        "",
+        "An arrange of her theme from Hidden Star in Four Seasons."
+    ],
+    "3": [
+        "@",
+        "　",
+        "Kirisame Marisa's and the Story Mode's midpoint theme.",
+        "",
+        "With a suspicious feel to it, this song makes you feel like you're dashing.",
+        "It's meant to fit pretty much any character while retaining Marisa's image.",
+        "It's a little ominous, but that's also the way this game is."
+    ]
+}
