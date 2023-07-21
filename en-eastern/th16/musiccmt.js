b35387ff35933cdd87032f4340226d62b10a1108
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:57:31 +0200

    Add files via upload

---
+++ en-eastern/th16/musiccmt.js
@@ -0,0 +1,62 @@
+{
+    "5": [
+        "@",
+        "",
+        "This is Sakata Nemuno's theme.",
+        "",
+        "I think this theme turned out particularly video-gamey.",
+        "Yikes, it's a yamanba. They show up often in folk tales, but in spite of that",
+        "they're a pretty hard creature to pin down.",
+        "Using eerieness and hot-blooded fervor as my base, I made a Touhou-style theme."
+    ],
+    "7": [
+        "@",
+        "",
+        "This is Komano Aunn's theme.",
+        "",
+        "A feeling of spring, bravery, and a somewhat-strong opponent, as well as that of a mascot.",
+        "It's an all-over-the-place theme with all those things crammed into it.",
+        "Her character concept is \"a single divine beast acting as a pair,\" but she doesn't have",
+        "a split personality or anything. Also, you can't see it, but she's got herself a tail!"
+    ],
+    "9": [
+        "@",
+        "",
+        "This is Yatadera Narumi's theme.",
+        "",
+        "I composed it with an image of someone with her feet planted firmly on the ground. The original ",
+        "Jizo Bodhisattva is an honorable figure who provides salvation in the long era until Maitreya ",
+        "appears, but at some point or another, a countless number of eccentric Jizo statues spread ",
+        "throughout all of Japan. So I figure it'd be nice if there was a magic Jizo somewhere in there."
+    ],
+    "11": [
+        "@",
+        "",
+        "This is Teireida Mai and Nishida Satono's theme.",
+        "",
+        "I incorporated a great deal of things into this song: eeriness, lightheartedness,",
+        "strong hostility, innocence, and on top of all that, a midboss-esque feeling.",
+        "I put enough in for both characters, so it's an extravagant song in a number of ways. ",
+        "Created with an image of them dancing madly and eerily."
+    ],
+    "13": [
+        "@",
+        "",
+        "This is Matara Okina's theme.",
+        "",
+        "A very \"This is the climax!\" sort of song with a lot of strength to it.",
+        "It has a strong melody, even compared to the other songs in the game, so it might've",
+        "been nice if I just went all-in and started singing lyrics to it. It's very likely that ",
+        "you'd start laughing too hard to even play the game, but that might actually be the best outcome."
+    ],
+    "15": [
+        "@",
+        "",
+        "The theme for Matara Okina(True).",
+        "",
+        "Even though it's an Extra Stage, it wound up being part of the main story.",
+        "Since it's your true battle with Okina, I swept away all the refreshing feeling of nature,",
+        "but still made it a song with a strong melody befitting of the true final boss.",
+        "Honestly, getting attacked from behind is just the worst!"
+      ]
+}
