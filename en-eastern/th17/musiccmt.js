cd4bfb89a27d9f93e186d8320f89141d9e69f535
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 12:04:02 +0200

    Add files via upload

---
+++ en-eastern/th17/musiccmt.js
@@ -0,0 +1,73 @@
+{
+    "3": [
+        "@",
+        "",
+        "This is Ebisu Eika's theme.",
+        "",
+        "It's a theme for a miscarried child with a knack for stacking stones.",
+        "The song is quite cute, opening with a dark intro but continuing",
+        "into a joyful, childish melody.",
+        "I also tried to give it a floaty feeling, like that of a jellyfish."
+    ],
+    "5": [
+        "@",
+        "",
+        "This is Ushizaki Urumi's theme.",
+        "",
+        "I aimed for a song that's dark, but not terrifically dark.",
+        "As you can see, she's an \"older sister\" sort of character,",
+        "so I added some laid-back atmosphere too.",
+        "I think it turned out as a good representation of the game as a whole."
+    ],
+    "7": [
+        "@",
+        "",
+        "This is Niwatari Kutaka's theme.",
+        "",
+        "A chicken god. Chickens tend to either be food or the targets of insult in both reality and fiction, ",
+        "but in this world, she seems to be a considerably honored god. I took a \"rushing forth\" ",
+        "feeling reminiscent of a chicken's walk, mixed it with the feeling of a formidable mid-game boss ",
+        "that brings a sudden difficulty spike, and whipped them together in a style fitting for this game. ",
+        "Bon appetit."
+    ],
+    "9": [
+        "@",
+        "",
+        "This is Kicchou Yachie's theme.",
+        "",
+        "She's a legendary creature that's like a cross between a dragon and a tortoise.",
+        "This one turned out as a mysterious song that can give you both a cool impression",
+        "or a cute impression, a lighthearted one or a heavy one. Since she's one of the game's",
+        "key characters, I added a bit of flair to it despite her midgame position."
+    ],
+    "11": [
+        "@",
+        "",
+        "This is Joutouguu Mayumi's theme.",
+        "",
+        "She's a haniwa soldier. I loved that NHK program with the haniwa costume",
+        "back in the day; I can remember it so clearly. I wanted to make a cool haniwa",
+        "this time, but \"oi, Hanimaru! ♪ Prince Hanimaru\" kept playing in my head the",
+        "whole time. The song is the usual midboss-ey fare, but it goes pretty hard."
+    ],
+    "13": [
+        "@",
+        "",
+        "This is Haniyasushin Keiki's theme.",
+        "",
+        "She's a genius sculptor. My only theme for this one was \"how cool can I make this boss",
+        "even though she's fighting in a work apron while holding sculpting tools?\" (Lol)",
+        "Since you end up fighting her without either the player or protagonist knowing who the",
+        "real bad guy is, it's a refreshing and somewhat-somber song with no feeling of hostility."
+    ],
+    "15": [
+        "@",
+        "",
+        "This is Kurokoma Saki's theme.",
+        "",
+        "She's Kurokoma. Since she's apparently a total meathead, her personality doesn't",
+        "seem all that bad. She exudes the most sense of security in the whole Animal Realm.",
+        "I tried to give her a cowgirl-esque outfit since she's a horse, but when you give it a",
+        "good, long think (or even just a short one), that's what her rider should be wearing."
+    ]
+}
