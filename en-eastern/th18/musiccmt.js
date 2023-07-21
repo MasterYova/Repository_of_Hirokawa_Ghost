f83e28cf5546f91b261f6f2c06dfd8efdd3f4efe
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 12:10:46 +0200

    Add files via upload

---
+++ en-eastern/th18/musiccmt.js
@@ -0,0 +1,70 @@
+{
+    "3": [
+        "@",
+        "",
+        "This is Goutokuji Mike's theme.",
+        "",
+        "When you think of floods of customers and prosperous business, you think of the maneki-neko. ",
+        "I think this track's melody feels sort of Japanese, or sort of Chinese-- ",
+        "it's got an Oriental vibe of sorts. A bouncing, cat-like track. Cute, don't you think?"
+    ],
+    "5": [
+        "@",
+        "",
+        "This is Yamashiro Takane's theme.",
+        "",
+        "She's one of the yamawaro, the mountain youkai that serve as counterparts to the kappa. ",
+        "As befits an intellectually-leaning youkai(?), this track came out with the feeling that she's",
+        "fighting you as if playing a game. It's an interesting track, with a nostalgic mood to it somewhere."
+    ],
+    "7": [
+        "@",
+        "",
+        "This is Komakusa Sannyo's theme.",
+        "",
+        "She's called Komakusa-Dayuu thanks to how she dresses, and the pipe she holds in one hand.",
+        "I composed a track with a strong, straightforward sense of individuality to it.",
+        "I did try to make something more yakuza-ish, though. What's up with this \"midboss\" feeling...?",
+        "Well, she is just serving as the gatekeeper; she's a midboss through and through."
+    ],
+    "9": [
+        "@",
+        "",
+        "This is Tamatsukuri Misumaru's theme.",
+        "",
+        "I composed this track with an image of being confronted in the depths of a dungeon by a",
+        "mysterious individual who may be an enemy or a friend. That's basically it (*laugh*).",
+        "Now then, Izanagi Objects suddenly came out of nowhere, but they show up in the music CD of the",
+        "same name that I made, so please take a look at that as well. Essentially a mysterious substance."
+    ],
+    "11": [
+        "@",
+        "",
+        "This is Iizunamaru Megumu's theme.",
+        "",
+        "As befits an important tengu, this theme is melancholic while still retaining a jokey feel.",
+        "The main phrase of the theme is nostalgic for me personally, ",
+        "and for some reason it makes my eyes tear up. I wonder if it's because",
+        "we rarely see themes with game-like phrases like this anymore."
+    ],
+    "13": [
+        "@",
+        "",
+        "This is Tenkyuu Chimata's theme.",
+        "",
+        "She's a market god. A rainbow is a place where Otherworlds intersect with ours.",
+        "Those very places where different things cross paths are her territory.",
+        "Her theme likewise became one where two sections intersect: a fun and lively section",
+        "with a light rhythm to it, and a powerful section that might somehow sound like wailing."
+    ],
+    "15": [
+        "@",
+        "",
+        "This is Himemushi Momoyo's theme.",
+        "",
+        "She's a centipede princess. Y'know, no matter how much I like insects, centipedes are...",
+        "yikes. They're hideous, poisonous, fairly familiar, and low-rarity. They're packed",
+        "with nothing but detestable qualities, and yet, I still hate them. Her theme, however,",
+        "turned out to be supremely fun, and just as cool as centipedes are. Maybe I do like them after all."
+    ]
+}
