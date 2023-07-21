9ffe18ab1f935a523d32013b25096df53a812dfa
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:49:12 +0200

    Add files via upload

---
+++ en-eastern/th13/musiccmt.js
@@ -0,0 +1,53 @@
+{
+    "3": [
+        "@",
+        "　",
+        "Saigyouji Yuyuko's theme.",
+        "",
+        "Not really an easy piece to classify.",
+        "Since I had to bring out the fact that she's only a first stage boss,",
+        "I stopped myself from arranging a previous song of hers.",
+        "In this game she just acts as a \"nameless npc villager\" who helps the protagonists on their way."
+    ],
+    "5": [
+        "@",
+        "　",
+        "Kasodani Kyouko's theme.",
+        "Do people still shout \"Yahoo\" when they go to the mountains?",
+        "Or do you feel embarrassed all of a sudden and can't find the courage to shout?",
+        "Well of course the Yamabiko get bored, it's lucid for them to go in to Buddhism from the boredom.",
+        "By the way, if you try to relieve stress by shouting \"Idiot!\" out toward the open sea,",
+        "you'll startle the fishermen."
+    ],
+    "7": [
+        "@",
+        "　",
+        "Miyako Yoshika's theme.",
+        "",
+        "Although she's a Jiang Shi, she was based on a zombie, so her theme's",
+        "introduction is a little exotic. But then as I was writing it I noticed",
+        "that the introduction has something of an Egyptian feel, sort of like a pyramid or something.",
+        "But that would be a mummy, not a zombie..."
+    ],
+    "9": [
+        "@",
+        "　",
+        "Kaku Seiga's theme.",
+        "",
+        "She's a wicked hermit. Kind of like a magician that does a large assortment of tricks.",
+        "So, I aimed for a suspicious song where you never know what's going to fly out at you next.",
+        "Being able to pass through walls would be useful for running away,",
+        "but it's not too useful for fighting, is it? It'd be a critical hit to a nurikabe, though."
+    ],
+    "15": [
+        "@",
+        "　",
+        "Futasuiwa Mamizou's theme.",
+        "",
+        "It's full of that old tanuki feeling.",
+        "Both foxes and tanuki are seen as tricksters, but of the two, tanuki seem more laid-back, right?",
+        "So, they're probably not well suited toward danmaku battles, but",
+        "as expected, the danmaku morphed into something else and..."
+      ]
+
+}
