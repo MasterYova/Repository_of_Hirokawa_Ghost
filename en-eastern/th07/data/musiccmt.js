8189c1b7bd39e230835869fc2b5e641759b95afe
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 10:57:28 +0200

    Add files via upload

---
+++ en-eastern/th07/data/musiccmt.js
@@ -0,0 +1,34 @@
+{
+    "11": [
+        "@",
+        "This is Konpaku Youmu's theme. I tried to express both \"A Tough Opponent\" and",
+        "\"Childishness\" with this theme. Perhaps this is the most boss-like theme in this game.",
+        "While it uses guitars, I gave up trying to make it sound like rock music almost",
+        "immediately (laugh). The hook's melody resembles the youkai."
+    ],
+    "13": [
+        "@",
+        "This is Saigyouji Yuyuko's theme.",
+        "I gave this theme an oriental and melodious feeling while making it the \"final\" song.",
+        "Having said that, the style of music is my usual fare. (laugh)",
+        "Vehement yet elegant, powerful but ephemeral; I thought I'd make this theme",
+        "quite emotional."
+    ],
+    "16": [
+        "@",
+        "This is Yakumo Ran's theme.",
+        "I composed it to be more dazzling than fantastic.",
+        "Hopefully, you'll get the sense of the energetic youkai from the song.",
+        "In my opinion, when there's a long fight in a shooting game, the music should",
+        "repeat and sustain its tempo. That is, it ought to be like this theme."
+    ],
+    "18": [
+        "@",
+        "This is Yakumo Yukari's theme.",
+        "Meant to be an arranged version of Ran's theme, it ended up quite different.",
+        "This one has an ominous feeling. Or rather, she's the very embodiment",
+        "of ominous. Even compared with the past characters, she has a devious",
+        "appearance, a dubious character, and thoughtless danmaku.",
+        "The theme's just like her: devious. (laugh)"
+    ]
+}
