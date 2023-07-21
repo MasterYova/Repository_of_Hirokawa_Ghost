b14f7cf75229d2678015a08d389492cf9762ee64
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:09:21 +0200

    Add files via upload

---
+++ en-eastern/th10/musiccmt.js
@@ -0,0 +1,71 @@
+{
+    "3": [
+        "@",
+        "　",
+        "This is Aki Minoriko's theme.",
+        "",
+        "It has a pretty good tempo compared to the rest. I tried making it with the image of a harvest",
+        "festival and an inexperienced and thoughtless god. The only game that can do with this kind of",
+        "tensionless piece is Touhou. Normally, fighting a boss with this kind of music would be difficult.",
+        "It's a short loop, but the boss fight itself is short anyway."
+    ],
+    "5": [
+        "@",
+        "　",
+        "This is Kagiyama Hina's theme.",
+        "",
+        "Since she's a Gothic lolita, I thought I should make it a more painful kind of feeling, but when I",
+        "thought of my own preferences, and the matter of the entire game, this kind of feeling just settled in.",
+        "",
+        "Well, she's just a Stage 2 boss. It's better to make her look a bit stupid."    ],
+    "7": [
+        "@",
+        "　",
+        "This is Kawashiro Nitori's theme.",
+        "You'll hear this song quite a bit, so I've put forth my best effort so that you won't get tired of it.",
+        "For that purpose, I've used some strange phrases and rhythms, but I wonder if it was any good.",
+        "My hypothesis is that you'll be listening to this one a lot. Since my first impressions of the song,",
+        "as I continued to listen to it, it seems to have gradually changed on me.",
+        "As the concluding piece, it should end up having a rather sorrowful sound to it, I guess?"
+    ],
+    "9": [
+        "@",
+        "　",
+        "This is Shameimaru Aya's theme.",
+        "",
+        "I made the same Aya's theme, Wind God Girl, with the image of Aya the newspaper reporter,",
+        "but this time I emphasized the image of the youkai \"Tengu\" from ancient Japan.",
+        "If I somehow gave it an old, nostalgic feeling, you too would become one of the youkai.",
+        "Ahh, it feels so nostalgic."
+    ],
+    "11": [
+        "@",
+        "　",
+        "This is Kochiya Sanae's theme.",
+        "It feels like the metal section suddenly got bigger here; I thought I'd give this a heavy feeling,",
+        "but I got careless and overdid it, which is a bad habit of mine. Tee hee.",
+        "Whether it's the melody or something else, it's the heaviest in this game.",
+        "From this point on, it suddenly becomes bright, and at the end it kind of lost its touch...",
+        "The stronger they are, the less the feeling of tension: This is the promise in Gensokyo."
+    ],
+    "13": [
+        "@",
+        "　",
+        "This is Yasaka Kanako's theme.",
+        "This is a strange god who makes all human and youkai her friends with an overwhelming power.",
+        "I composed a part which makes you feel awe toward a god, and a part which makes you feel",
+        "the gravity of history.",
+        "Anyhow, it's a playful tune which possesses ample reckless abandon. There's a huge difference",
+        "compared to Sanae in the melody, but maybe this is the difference between gods and humans."
+    ],
+    "15": [
+        "@",
+        "　",
+        "This is Moriya Suwako's theme.",
+        "It's a cheerful piece with a rapid feeling. Moreover, I tried to add a Japanese and childish",
+        "feeling to the melody. It fits Suwako perfectly.",
+        "Such a lively tune is on the contrary, actually a piano piece from hell.",
+        "Again, this song has an abundance of chaos, even more so than Kanako. You'd definitely think",
+        "she was playing around with the shrine maiden, what with the particularly high speed piano."
+    ]
+}
