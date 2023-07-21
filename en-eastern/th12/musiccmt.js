91c4a4f1b1588daeba327e49263cac6461f4e3e6
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:44:46 +0200

    Add files via upload

---
+++ en-eastern/th12/musiccmt.js
@@ -0,0 +1,62 @@
+{
+    "5": [
+        "@",
+        "　",
+        "This is Tatara Kogasa's theme.",
+        "",
+        "Most people probably buy quite a few umbrellas over their lives.",
+        "You forget them on trains, and you buy vinyl umbrellas when you're caught in sudden rain.",
+        "Anyway, I wrote this song based on the image of a self-centered karakasa youkai",
+        "who gets depressed easily despite her cheery nature."
+    ],
+    "7": [
+        "@",
+        "　",
+        "This is Kumoi Ichirin & Unzan's theme.",
+        "",
+        "Since they're pretty fun characters, I tried to make it fun and old-fashioned.",
+        "It's really nice, sometimes silly and sometimes sentimental.",
+        "Unzan is made out of clouds, so don't worry about it if more than two",
+        "of him or four of his arms show up at once. Flash of insight!"
+    ],
+    "9": [
+        "@",
+        "　",
+        "This is Murasa Minamitsu's theme.",
+        "",
+        "Since she sinks lots of ships, she's a well-known ghost captain.",
+        "I tried to give it a \"captain\" atmosphere, so it's a little too cool.",
+        "Everyone calls her Murasa, but \"murasa\" is the name of a youkai,",
+        "so they're not really calling her by name - surprise!"
+    ],
+    "11": [
+        "@",
+        "　",
+        "This is Toramaru Shou's theme.",
+        "",
+        "I gave this song a quiet, but strong feel. The second half is awfully boss-ish.",
+        "It's a feeling of dignity rather than terror.",
+        "Still, those lasers coming one after the other are pretty scary.",
+        "They're old-man killers you need really good reflexes to dodge."
+    ],
+    "13": [
+        "@",
+        "　",
+        "This is Hijiri Byakuren's theme.",
+        "",
+        "From the quiet heartbeat, suddenly it feels like the world's opening up.",
+        "This is the theme of a last boss who's hard to tire out, probably.",
+        "It's overall fairly melodiously Japanese, and I tried to make the melody of the",
+        "main part like one of those songs where everyone's singing. Human, bare your feelings and GO!"
+    ],
+    "15": [
+        "@",
+        "　",
+        "This is Houjuu Nue's theme.",
+        "",
+        "A vigorously youkai youkai song.",
+        "It's a song with an old-fashioned, Japanese sense of terror and sorrow floating about.",
+        "Isn't it really fitting for an EX boss?",
+        "The mostly one-shot recording full of ad-libbing is also pretty EX-ish."
+    ]
+}
