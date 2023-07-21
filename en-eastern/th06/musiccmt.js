6743620f3c477965784941246ed025a8fd5b773e
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 10:55:49 +0200

    Add files via upload

---
+++ en-eastern/th06/musiccmt.js
@@ -0,0 +1,11 @@
+{
+    "11": [
+        "@",
+        "Izayoi Sakuya's theme.",
+        "You can't have a maid without hard rock (joking)",
+        "It's quite driving, fitting for the second-to-last stage.",
+        "",
+        "...Guess I still don't know much about rock (^^;",
+        "Let's just say it's poetic rock, done my way."
+    ]
+}
