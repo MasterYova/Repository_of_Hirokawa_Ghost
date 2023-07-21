09c00dfeb85ce0d2e3c47737f6e000f2c3aba1ff
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:42:07 +0200

    Add files via upload

---
+++ en-eastern/th11/musiccmt.js
@@ -0,0 +1,63 @@
+{
+    "3": [
+        "@",
+        "　",
+        "This is Kurodani Yamame's theme.",
+        "",
+        "I thought I'd try making it cool for a stage one song.",
+        "It's a very simple song with that particular game music feel to it.",
+        "I think it's best if a simple melody helps bring the game",
+        "action to a climax."
+    ],
+    "5": [
+        "@",
+        "　",
+        "This is Mizuhashi Parsee's theme.",
+        "",
+        "She's a youkai with a wounded heart.",
+        "So, I thought I'd like to make her a melodious song, and this is how it ended up.",
+        "You wouldn't think it's a song for a shooting game, but when I tried to think",
+        "of something good for a shooting game it settled down into something like this."
+    ],
+    "7": [
+        "@",
+        "　",
+        "This is Hoshiguma Yuugi's theme.",
+        "",
+        "I was aiming for a strong, very playful image.",
+        "In fact, Yuugi is teaching herself how to fight without spilling",
+        "a drop of the saké from her cup. She must be bored.",
+        "Somehow, I think the oni of Hell have more fun than humans."
+    ],
+    "9": [
+        "@",
+        "　",
+        "This is Komeiji Satori's theme.",
+        "",
+        "She's the master of the Palace of the Earth Spirits. She's a little fancy and loves animals.",
+        "Actually, she should be the highest ranked enemy in Subterranean Animism,",
+        "but it looks like fighting isn't her specialty.",
+        "I tried to make the song dramatic and otherworldly, too."
+    ],
+    "13": [
+        "@",
+        "　",
+        "This is Reiuji Utsuho's theme.",
+        "",
+        "I tried to make it as modern, light, and simply boss-like as I could.",
+        "In fact, she's not that great a youkai, for a last boss. She was originally just a bird.",
+        "She's just really, really powerful. But I understand very well that even a fool that",
+        "possesses great power can't cause too much harm. Ah, even if it's the power of dreams."
+    ],
+    "15": [
+        "@",
+        "　",
+        "This is Komeiji Koishi's theme.",
+        "",
+        "I tried to make it eerie and have the aura of a broken girl.",
+        "I think the eeriness is fitting for an extra boss.",
+        "Actually, I think a garden with roses strewn all about where thorns can prick",
+        "you is really fitting for a battle, but for some reason it's the shrine this time."
+      ]
+
+}
