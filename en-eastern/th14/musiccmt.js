112b796785ee27dad223324052e9d9b0cb370f3e
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-07-21 11:52:47 +0200

    Add files via upload

---
+++ en-eastern/th14/musiccmt.js
@@ -0,0 +1,52 @@
+{
+    "7": [
+        "@",
+        "　",
+        "Imaizumi Kagerou's theme.",
+        "",
+        "I tried giving it a \"middle of the game boss\" feeling.",
+        "Well, it's stage 3 anyway.",
+        "",
+        "By the way, since this full moon is real, she can transform, too."
+      ],
+    "9": [
+        "@",
+        "　",
+        "Tsukumo Benben & Tsukumo Yatsuhashi 's theme.",
+        "I had wanted to make it a more clichéd kind of traditional Japanese song,",
+        "but if you let the songs be too artsy, the atmosphere of the game is ruined.",
+        "So I resisted.",
+        "Though I did make it sort of like that, but I didn't have a sample for a biwa,",
+        "so I'm a bit depressed about that."
+    ],
+    "11": [
+        "@",
+        "　",
+        "Kijin Seija's theme.",
+        "",
+        "I wrote this while thinking of the image of a little, lazy youkai.",
+        "The result is pretty mischievous... well, the mood's pretty boss-like, so I'm happy.",
+        "This is a little boss that would take the initiative in disliking others.",
+        "Even so, she's the villain this round."
+    ],
+    "13": [
+        "@",
+        "　",
+        "Sukuna Shinmyoumaru's theme.",
+        "",
+        "I made it a cool song fit for a small body.",
+        "It's not very heavy, but since it's a last boss, I gave it a",
+        "nice expansion, and I think it feels good as a result.",
+        "Huh? She's not small? Well, then please beat the game."
+    ],
+    "15": [
+        "@",
+        "　",
+        "Horikawa Raiko's theme.",
+        "I wanted to make the song feature drums prominently, but",
+        "in the end I just didn't have the courage. In terms of the game's image.",
+        "The drums play a main part in a little bit of the song.",
+        "On a side note, her shoes are probably structured so she can tap her feet",
+        "to hit the bass drum."
+    ]
+}
