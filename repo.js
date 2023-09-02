4048ea9ccc73e8080b5e288c93c0cd8e409005e2
MasterYova <137089094+MasterYova@users.noreply.github.com>
2023-09-02 15:37:42 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -9,8 +9,8 @@
         "https://srv.thpatch.net/"
     ],
     "patches": {
-        "en-eastern": "Japanese characters' names in the eastern name order(last name first) for the english patch",
-        "kuchko": "Prevod Alisine i Marisine \"bitch\" rečenice na srpski(latinicu) u TH08.",
-        "jouga": "Changes Chang'e into Jouga for those who like Japanese names more than the Chinese ones."
-      }
+		"en-eastern": "Japanese characters' names in the eastern name order(last name first) for the english patch",
+		"jouga": "Replaces Chang'e with Jouga in LOLK(TH15).",
+		"kuchko": "Prevod Alisine i Marisine \"bitch\" rečenice na srpski(latinicu) u TH08."
+	},
 }
