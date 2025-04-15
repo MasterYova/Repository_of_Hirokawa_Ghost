8433f6e8894c739881dfc2552bc8fad763fd058f
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-04-15 18:11:59 +0200

    Create patch.js

---
+++ shikieiki-sr/patch.js
@@ -0,0 +1,10 @@
+{
+	"id": "shikieiki",
+	"servers": [
+		"https://mirrors.thpatch.net/yova/shikieiki-sr"
+    ],
+		"dependencies": [
+                "nmlgc/base_tsa",
+  ],
+	"title":"Spaja Šiki Eiki nazad u Šikieiki(srpski prevod)."
+}
