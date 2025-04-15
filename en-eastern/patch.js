dd1220eeb661637012ac0d52509c3fcaf2ae26d5
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-04-15 17:53:32 +0200

    Update patch.js

--- en-eastern/patch.js
+++ en-eastern/patch.js
@@ -4,9 +4,6 @@
     "https://mirrors.thpatch.net/yova/en-eastern/"
   ],
   "dependencies": [
-    "nmlgc/base_tsa",
-    "nmlgc/base_tasofro",
-    "nmlgc/script_latin",
     "thpatch/lang_en",
     "nmlgc/eastern_name_order_hepburn"
   ],
