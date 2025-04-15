8dc6bac1dd55b9f0fd2769b5b2f38b9b4c67cae6
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-04-15 16:50:00 +0200

    Create patch.js

---
+++ shikieiki/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "shikieiki",
+	"servers": [
+		"https://mirrors.thpatch.net/yova/shikieiki"
+    ],
+	"title":"Merges Shiki Eiki's name back to Shikieiki."
+}
