e75e94400903ceebc51163c74e85371634417fb8
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-04-15 17:57:31 +0200

    Update patch.js

--- jouga/patch.js
+++ jouga/patch.js
@@ -1,10 +1,10 @@
 {
-	"dependencies": [
-		"thpatch/lang_en"
-	],
 	"id": "jouga",
 	"servers": [
 		"https://mirrors.thpatch.net/yova/jouga"
     ],
+	"dependencies": [
+		"thpatch/lang_en"
+	],
 	"title":"Replaces Chang'e with Jouga in LOLK(TH15)."
 }
