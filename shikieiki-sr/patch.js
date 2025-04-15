ce233be283157b2fe428ac7c56b5b6fcd5c20e3e
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-04-15 18:29:20 +0200

    Update patch.js

--- shikieiki-sr/patch.js
+++ shikieiki-sr/patch.js
@@ -4,7 +4,7 @@
 		"https://mirrors.thpatch.net/yova/shikieiki-sr"
     ],
 		"dependencies": [
-                "nmlgc/base_tsa",
+                "nmlgc/base_tsa"
   ],
 	"title":"Spaja Šiki Eiki nazad u Šikieiki(srpski prevod)."
 }
