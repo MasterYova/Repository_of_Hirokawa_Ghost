0e8d41acdfb03c6d9c15e179d9450ff3ae3e8016
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-05-20 12:41:31 +0200

    Create patch.js

---
+++ shikieiki-common/patch.js
@@ -0,0 +1,11 @@
+{
+	"id": "shikieiki-common",
+	"servers": [
+		"https://mirrors.thpatch.net/yova/shikieiki-common"
+    ],
+		"dependencies": [
+                "nmlgc/base_tsa",
+                "nmlgc/base_tasofro"
+  ],
+	"title":"Common resources for shikieiki patches."
+}
