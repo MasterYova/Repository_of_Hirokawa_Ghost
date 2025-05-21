7de35154e8aa03743d5b62e3c5c9f7a700455611
MasterYova <137089094+MasterYova@users.noreply.github.com>
2025-05-21 11:52:39 +0200

    Update patch.js

--- shikieiki-en-literal/patch.js
+++ shikieiki-en-literal/patch.js
@@ -4,8 +4,8 @@
 		"https://mirrors.thpatch.net/yova/shikieiki-en-literal"
     ],
 		"dependencies": [
-		            "yova/shikieiki",
-                            "thpatch/lang_en-literal"
+		            "thpatch/lang_en-literal",
+		            "yova/shikieiki"
   ],
 	"title":"Merges Shiki Eiki's name back to Shikieiki(for the Literal English translation)."
 }
